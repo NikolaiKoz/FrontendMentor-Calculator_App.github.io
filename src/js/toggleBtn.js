ee94da03d578399c279c0484db7d7608d6b5e7b9
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2022-12-18 20:18:47 -0300

    feat: :sparkles: The switch now can move when the user clicks over him.

--- src/js/toggleBtn.js
+++ src/js/toggleBtn.js
@@ -6,20 +6,30 @@ toggleBtn.addEventListener('click', () => {
 
 const ball = document.querySelector('.switchOptions__switch__backgorund__circle');
 
+let ballClass = "";
+
     if (ball.classList.contains('circlePosition1')) {
-        ball.classList.remove('circlePosition1');
-        ball.classList.add('circlePosition2');
+        ballClass = 'circlePosition1';
+    } else if (ball.classList.contains('circlePosition2')) {
+        ballClass = 'circlePosition2';
+    } else if (ball.classList.contains('circlePosition3')) {
+        ballClass = 'circlePosition3';
     }
 
-    if (ball.classList.contains('circlePosition2')) {
-        ball.classList.remove('circlePosition2');
-        ball.classList.add('circlePosition3');
-    }
-
-    if (ball.classList.contains('circlePosition3')) {
-        ball.classList.remove('circlePosition3');
-        ball.classList.add('circlePosition1');
-    }
+    switch (ballClass) {
+        case 'circlePosition1':
+            ball.classList.remove('circlePosition1');
+            ball.classList.add('circlePosition2');
+            break;
+        case 'circlePosition2':
+            ball.classList.remove('circlePosition2');
+            ball.classList.add('circlePosition3');
+            break;
+        case 'circlePosition3':
+            ball.classList.remove('circlePosition3');
+            ball.classList.add('circlePosition1');
+            break;
+    };
 
 });
 
